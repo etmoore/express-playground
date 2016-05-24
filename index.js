09f6fbf015d6502f6276168cdb46e40f8acfaff8
Evan Moore <etmoore@gmail.com>
2016-05-23 22:25:28 -0600

    add route handler examples D and books

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ app.delete('/user', function(req, res){
   res.send('Got a DELETE request at /user');
 });
 
+// route handler example D
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+}
+
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+}
+
+app.get('/example/d', [cb0, cb1], function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from D!');
+});
+
+// book example
+app.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
+// start the server
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
 });
